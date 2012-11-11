913aa4a1ed0bcff24d40c85757eebdb3f5252b26
Isaac <hyperneato@gmail.com>
2012-11-11 15:33:07 -0800

    Change default test database name.

--- tests/test_queryset.py
+++ tests/test_queryset.py
@@ -191,7 +191,7 @@ class TestRedisQueryset(unittest.TestCase):
 
     def setUp(self):
         import redis
-        redis_connection = redis.StrictRedis(host='localhost', port=6379, db=0)
+        redis_connection = redis.StrictRedis(host='localhost', port=6379, db='test_queryset.TestRedisQueryset')
         redis_connection.flushdb()
         self.queryset = RedisQueryset(db_conn=redis_connection)
 
